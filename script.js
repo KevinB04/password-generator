23cb61dd8ca93c8d2a74d8c51b730729bc9a0bf1
Kevin Buttimer <kbuttimer4@gmail.com>
2020-12-09 22:22:07 -0500

    updating javascript file

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log("Hello World!")
 
 var generateBtn = document.querySelector("#generate");
 var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var  lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numericArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "|", "~"];
 var passwordArray = [];
@@ -14,14 +14,11 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  
-
 }
 
 function generatePassword() {
 
 
-  
   if (lengthOfPassword > 8 || lengthOfPassword < 128) {
     var lengthOfPassword = prompt("Choose a password between 8 and 128 characters");
   }
@@ -35,7 +32,7 @@ function generatePassword() {
     Math.floor(Math.random() * passwordArray.length)
   }
 
-  var lengthOfPassword = prompt("Choose a password length from 8-128");
+  var lengthOfPassword = prompt("Choose a password length between 8-128 characters");
   console.log(lengthOfPassword);
 
 
